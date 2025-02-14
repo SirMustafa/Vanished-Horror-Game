9cd7f3faa1d220fa0a0ef8c5f9d62b5c5e5f4b42
Mustafa <79559036+SirMustafa@users.noreply.github.com>
2025-02-14 09:57:46 +0300

    models lightmap settings

--- Assets/Scripts/MeshCombiner.cs
+++ Assets/Scripts/MeshCombiner.cs
@@ -5,55 +5,29 @@ using UnityEngine;
 
 public class MeshCombiner : MonoBehaviour
 {
-    // Source Meshes you want to combine
-    [SerializeField] private List<MeshFilter> listMeshFilter;
-
-    // Make a new mesh to be the target of the combine operation
-    [SerializeField] private MeshFilter TargetMesh;
-
-    [ContextMenu("Combine Meshes")]
-    private void CombineMesh()
+    [RequireComponent(typeof(MeshFilter))]
+    [RequireComponent(typeof(MeshRenderer))]
+    public class CombineMesh : MonoBehaviour
     {
-        //Make an array of CombineInstance.
-        var combine = new CombineInstance[listMeshFilter.Count];
-
-        //Set Mesh And their Transform to the CombineInstance
-        for (int i = 0; i < listMeshFilter.Count; i++)
+        void Start()
         {
-            combine[i].mesh = listMeshFilter[i].sharedMesh;
-            combine[i].transform = listMeshFilter[i].transform.localToWorldMatrix;
+            CombineInstance[] combine
+                = new CombineInstance[transform.childCount];
+
+            int index = 0;
+            foreach (Transform child in gameObject.transform)
+            {
+                MeshFilter filter = child.GetComponent<MeshFilter>();
+                combine[index].mesh = filter.sharedMesh;
+                combine[index].transform = child.localToWorldMatrix;
+                child.gameObject.SetActive(false);
+                index++;
+            }
+
+            Mesh mesh = new Mesh();
+            mesh.CombineMeshes(combine);
+            transform.GetComponent<MeshFilter>().sharedMesh = mesh;
+            transform.gameObject.SetActive(true);
         }
-
-        // Create a Empty Mesh
-        var mesh = new Mesh();
-
-        //Call targetMesh.CombineMeshes and pass in the array of CombineInstances.
-        mesh.CombineMeshes(combine);
-
-        //Assign the target mesh to the mesh filter of the combination game object.
-        TargetMesh.mesh = mesh;
-
-        // Save The Mesh To Location
-        SaveMesh(TargetMesh.sharedMesh, gameObject.name, false, true);
-
-        // Print Results
-        print($"<color=#20E7B0>Combine Meshes was Successful!</color>");
-    }
-
-
-    public static void SaveMesh(Mesh mesh, string name, bool makeNewInstance, bool optimizeMesh)
-    {
-        string path = EditorUtility.SaveFilePanel("Save Separate Mesh Asset", "Assets/", name, "asset");
-        if (string.IsNullOrEmpty(path)) return;
-
-        path = FileUtil.GetProjectRelativePath(path);
-
-        Mesh meshToSave = (makeNewInstance) ? Object.Instantiate(mesh) as Mesh : mesh;
-
-        if (optimizeMesh)
-            MeshUtility.Optimize(meshToSave);
-
-        AssetDatabase.CreateAsset(meshToSave, path);
-        AssetDatabase.SaveAssets();
     }
 }
